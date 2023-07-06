4718c37cb4871c2c28de745be6209c09b7dde4ba
brsmoraes <brunomores1@gmail.com>
2023-07-06 17:59:15 -0300

    Add files via upload

---
+++ redes/home/views.py
@@ -0,0 +1,35 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import JsonResponse
+import json
+
+import pandas as pd
+import warnings
+from sdv.tabular import GaussianCopula
+
+
+@api_view(['GET', 'POST'])
+def modelo(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        model = GaussianCopula.load('/home/bruno/Projetos/Disciplinas/Redes/aplicacao/redes/home/2010-2023.pk')
+
+        quantidade = request.query_params['quantidade']
+
+        retorno = model.sample(int(quantidade)).to_json()
+
+
+        return Response(json.loads(retorno))
+
+    """
+    elif request.method == 'POST':
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    """
